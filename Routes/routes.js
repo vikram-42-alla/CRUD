1b544442a85ff433ce4f8ebcc5be70cfb9b349d3
vikram-42-alla <allatrivikram@gmail.com>
2025-03-18 20:52:09 +0530

    commit

--- Routes/routes.js
+++ Routes/routes.js
@@ -12,4 +12,12 @@ router.post("/user",async(req,res)=>{
         res.status(500).json({error:error.message})
     }
 })
+router.get("/get",async(req,res)=>{
+    try{
+    const user=await User.findById(req.params.id)
+    if(!user) res.status(404).json({message:"User not found"})
+    }catch(error){
+res.status(500).json({message:error.message})
+    }
+})
 module.exports=router
